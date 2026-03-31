1e17dd2a548f4dd9de79c8b235d544d36f239874
Stéphane <contact@stephanefage.fr>
2026-03-31 15:58:15 +0200

    Ajout constructeur

---
+++ src/inventaire/Inventaire.java
@@ -0,0 +1,18 @@
+package inventaire;
+
+public class Inventaire {
+    Object[] objets;
+    Integer capaciteMax;
+    Integer capaciteActuelle;
+
+
+    public Inventaire(Object[] objets, Integer capaciteMax, Integer capaciteActuelle) {
+        this.objets = objets;
+        this.capaciteMax = capaciteMax;
+        this.capaciteActuelle = capaciteActuelle;
+    }
+
+    public ajouter () {
+
+    }
+}
